cd71f47c6d2c190cb73de6f2652a7342d3844e11
Eugenia Shibkova <shibkova91@gmail.com>
2021-05-30 20:11:37 +0300

    refactor: modify SearchItem component

--- src/components/Header/SearchItem/SearchItem.js
+++ src/components/Header/SearchItem/SearchItem.js
@@ -1,18 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './search-item.css';
 
-const SearchItem = () => (
-  <form className="search">
-    <div className="search__input-wrapper">
-      <input
-        type="text"
-        className="search__input"
-        name="text"
-        placeholder="Enter GitHub username"
-        autoComplete="off"
-      />
-    </div>
-  </form>
-);
+const SearchItem = () => {
+  const [searchValue, setSearchValue] = useState('');
+
+  function searchHandler(event) {
+    if (event.key === 'Enter') {
+      event.preventDefault();
+      console.log(event.target.value)
+      setSearchValue(event.target.value);
+    }
+    // dispatch(setCurrentPage(1))
+    // dispatch(getRepos(searchValue, currentPage, perPage))
+  }
+
+  return (
+    <form className="search">
+      <div className="search__input-wrapper">
+        <input
+          value={searchValue}
+          onChange={(e) => setSearchValue(e.target.value)}
+          onKeyDown={searchHandler}
+          type="text"
+          className="search__input"
+          name="text"
+          placeholder="Enter GitHub username"
+          autoComplete="off"
+        />
+      </div>
+    </form>
+  )
+};
 
 export default SearchItem;
